fc27c122b0b315b25cb48e64bdf44d6afeae7906
Max Kryzhanovskyi <maximkryzhanovsky@gmail.com>
2022-08-16 10:31:08 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { refs } from './refs';
-import { createMarkup, clearMarkup, hideSearchBtn } from './markup';
-import { fetchRequest, pageReset, toLoadMore, incPage } from './request';
+import { createMarkup, clearMarkup } from './markup';
+import { fetchRequest, toLoadMore } from './request';
 import { onSuccess, onError, onEnd } from './helpers';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -18,31 +18,22 @@ export async function onSubmit(event) {
   const value = event.target.elements.searchQuery.value.trim();
 
   if (!value) return;
-
-  const response = await fetchRequest(value);
-  if (response.data.hits.length === 0) {
-    loadMoreBtn.disabled = true;
-    return onError();
-  } else if (response.data.hits.length < 40) {
-    loadMoreBtn.disabled = true;
-    createMarkup(response.data.hits);
-    if (galleryRef.children.length === response.data.totalHits) {
-      loadMoreBtn.disabled = true;
-      lightbox.refresh();
-      return onEnd();
-    }
-    lightbox.refresh();
-    return onEnd();
-  } else {
-    createMarkup(response.data.hits);
-    loadMoreBtn.disabled = false;
-    onSuccess(response.data.totalHits);
-    lightbox.refresh();
-  }
-
   PAGE.value = 1;
 
-  //   console.dir(event.target.elements.searchQuery.value);
+  const response = await fetchRequest(value);
+  if (!response.data.hits.length) {
+    loadMoreBtn.disabled = true;
+    return onError();
+  }
+  if (response.data.totalHits < 40) {
+    loadMoreBtn.disabled = true;
+    onEnd();
+  }
+  loadMoreBtn.disabled = false;
+  onSuccess(response.data.totalHits);
+  createMarkup(response.data.hits);
+  lightbox.refresh();
+
 }
 
 export const lightbox = new SimpleLightbox('.gallery a', {
--- src/request/index.js
+++ src/request/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 import { BASE_URL, KEY, QUERY_PARAMS, PAGE } from '../service';
 import { refs } from '../refs';
-const { formRef, loadMoreBtn } = refs;
+const { formRef, galleryRef, loadMoreBtn } = refs;
 import { onEnd, onError } from '../helpers';
 import { createMarkup } from '../markup';
 import { lightbox } from '../';
@@ -12,27 +12,27 @@ export async function fetchRequest(request) {
   try {
     const url = `${BASE_URL}?key=${KEY}&q=${request}&${QUERY_PARAMS}&page=${PAGE.value}`;
     const response = await axios.get(url);
-    // console.log(response);
     return response;
   } catch (error) {
-    onError();
+    throw new Error();
   }
 }
 
 export async function toLoadMore(event) {
-  PAGE.value += 1;
-  const response = await fetchRequest(formRef[0].value);
-  console.log('test 1111', response.data.hits.length);
+  try {
+    PAGE.value += 1;
+    const response = await fetchRequest(formRef[0].value);
+    console.log('test 1111', response?.data?.hits?.length);
+    console.log(galleryRef.children.length);
 
-  if (response.data.hits.length < 40) {
-    loadMoreBtn.disabled = true;
-    createMarkup(response.data.hits);
-    if (galleryRef.children.length === response.data.totalHits) {
+    if (galleryRef.children.length >= response.data.totalHits) {
       loadMoreBtn.disabled = true;
-      lightbox.refresh();
       return onEnd();
     }
+    createMarkup(response.data.hits);
     lightbox.refresh();
+  } catch (error) {
+    loadMoreBtn.disabled = true;
     return onEnd();
   }
 }
